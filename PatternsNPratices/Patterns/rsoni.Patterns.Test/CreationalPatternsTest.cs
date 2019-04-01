3b526d1d3376c3b58c80dfc4c33ea94275576793
Rajesh Soni <talk2rsoni@outlook.com>
2019-04-01 21:57:10 +0530

    Updated Singleton

---
+++ PatternsNPratices/Patterns/rsoni.Patterns.Test/CreationalPatternsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rsoni.Patterns.Creational;
+
+namespace rsoni.Patterns.Test
+{
+    [TestClass]
+    public class CreationalPatternsTest
+    {
+        [TestMethod]
+        public void TestSingleton()
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            Assert.AreEqual(s1, s2);
+
+        }
+    }
+}
---
+++ PatternsNPratices/Patterns/rsoni.Patterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsoni.Patterns.Creational
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        // Constructor is 'protected'
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+
+            // Note: this is not thread safe.
+
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
