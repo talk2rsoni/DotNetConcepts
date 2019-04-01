92b97c427749caf4814f06a84368a0e32204b8ec
Rajesh Soni <talk2rsoni@outlook.com>
2019-04-01 18:36:11 +0530

    Updated all Codes

--- PatternsNPratices/SOLID/rsoni.SolidPrinciples/4.ISP/ISP.WrongImpl.cs
+++ PatternsNPratices/SOLID/rsoni.SolidPrinciples/4.ISP/ISP.WrongImpl.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace rsoni.SolidPrinciples.ISP.WrongImpl
 {
- 
-   
 
+    public interface ILead
+    {
+        void CreateSubTask();
+        void AssignTask();
+        void WorkOnTask();
+    }
+
+    public class TeamLead : ILead
+    {
+        public void AssignTask()
+        {
+            //Code to assign a task.  
+        }
+        public void CreateSubTask()
+        {
+            //Code to create a sub task  
+        }
+        public void WorkOnTask()
+        {
+            //Code to implement perform assigned task.  
+        }
+    }
+
+    public class Manager : ILead
+    {
+        public void AssignTask()
+        {
+            //Code to assign a task.  
+        }
+        public void CreateSubTask()
+        {
+            //Code to create a sub task.  
+        }
+        public void WorkOnTask()
+        {
+            throw new Exception("Manager can't work on Task");
+        }
+    }
 }
