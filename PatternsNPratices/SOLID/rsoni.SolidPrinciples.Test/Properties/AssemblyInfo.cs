216e23da39b1f4aeb6ce3070b3e38b2fb4808e05
Rajesh Soni <talk2rsoni@outlook.com>
2019-04-01 16:48:48 +0530

    Folder Structure changes

---
+++ PatternsNPratices/SOLID/rsoni.SolidPrinciples.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("rsoni.SolidPrinciples.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rsoni.SolidPrinciples.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("47e6b93f-fd12-4f5e-9997-fac7b2e4edba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- PatternsNPratices/SOLID/rsoni.SolidPrinciples/Properties/AssemblyInfo.cs
+++ PatternsNPratices/SOLID/rsoni.SolidPrinciples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SolidPrinciples")]
+[assembly: AssemblyTitle("rsoni.SolidPrinciples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SolidPrinciples")]
+[assembly: AssemblyProduct("rsoni.SolidPrinciples")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
