8c817669920999ef901ff0539eda9357afcffa0c
Rajesh Soni <talk2rsoni@outlook.com>
2019-04-01 17:36:15 +0530

    OCP Implemented

---
+++ PatternsNPratices/SOLID/rsoni.SolidPrinciples/2.OCP/OCP.CorrectImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsoni.SolidPrinciples.OCP.CorrectImpl
+{
+    public abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public override double Area()
+        {
+            return Height * Width;
+        }
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double Area()
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+
+    public class AreaCalculator
+    {
+        public double TotalArea(Shape[] arrShapes)
+        {
+            double area = 0;
+            foreach (var objShape in arrShapes)
+            {
+                area += objShape.Area();
+            }
+            return area;
+        }
+    }
+}
---
+++ PatternsNPratices/SOLID/rsoni.SolidPrinciples/2.OCP/OCP.WrongImpl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsoni.SolidPrinciples.OCP.WrongImpl
+{
+ 
+    #region  1st senario
+    public class Rectangle
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+    public class AreaCalculator
+    {
+        public double TotalArea(Rectangle[] arrRectangles)
+        {
+            double area = 0;
+            foreach (var objRectangle in arrRectangles)
+            {
+                area += objRectangle.Height * objRectangle.Width;
+            }
+            return area;
+        }
+    }
+    #endregion
+
+    #region  2nd senario
+    public class Circle
+    {
+        public double Radius { get; set; }
+    }
+    public class AreaCalculator1
+    {
+        public double TotalArea(object[] arrObjects)
+        {
+            double area = 0;
+            Rectangle objRectangle;
+            Circle objCircle;
+            foreach (var obj in arrObjects)
+            {
+                if (obj is Rectangle)
+                {
+                    objRectangle = (Rectangle)obj;
+                    area += objRectangle.Height * objRectangle.Width;
+                }
+                else
+                {
+                    objCircle = (Circle)obj;
+                    area += objCircle.Radius * objCircle.Radius * Math.PI;
+                }
+            }
+            return area;
+        }
+    }
+    #endregion
+
+}
